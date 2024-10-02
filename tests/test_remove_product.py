22e620dd5f85f29bdcac67d6a5d04ba84f2ebcdf
Bartemix836 <114190309+Bartemix836@users.noreply.github.com>
2024-10-02 23:48:57 +0200

    Update test_remove_product.py

--- tests/test_remove_product.py
+++ tests/test_remove_product.py
@@ -75,12 +75,12 @@ def test_user_login_checkout(setup_driver):
 
     # STEP1 - Enter the username:
     login_input_xpath = '//*[@id="user-name"]'
-    login_input = wait.until(EC.element_to_be_clickable((By.XPATH, login_input_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, login_input_xpath)))
     base_page.fill_text_field(By.XPATH, login_input_xpath, username)
 
     # STEP2 - Enter the password:
     passwd_input_xpath = '//*[@id="password"]'
-    passwd_input = wait.until(EC.element_to_be_clickable((By.XPATH, passwd_input_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, passwd_input_xpath)))
     base_page.fill_text_field(By.XPATH, passwd_input_xpath, password)
 
     # STEP3 - Press the login button:
@@ -94,26 +94,26 @@ def test_user_login_checkout(setup_driver):
 
     # STEP5 - Add products to the cart:
     add_btn1_xpath = '//*[@id="add-to-cart-sauce-labs-fleece-jacket"]'
-    add_product1 = wait.until(EC.element_to_be_clickable((By.XPATH, add_btn1_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, add_btn1_xpath)))
     base_page.click_element(By.XPATH, add_btn1_xpath)
 
     add_btn2_xpath = '//*[@id="add-to-cart-sauce-labs-backpack"]'
-    add_product2 = wait.until(EC.element_to_be_clickable((By.XPATH, add_btn2_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, add_btn2_xpath)))
     base_page.click_element(By.XPATH, add_btn2_xpath)
 
     # STEP6 - Go to the cart:
     cart_xpath = '//*[@id="shopping_cart_container"]/a'
-    go_cart = wait.until(EC.element_to_be_clickable((By.XPATH, cart_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, cart_xpath)))
     base_page.click_element(By.XPATH, cart_xpath)
 
     # STEP6A - Remove the selected product from the cart:
     removebtn_xpath = '//*[@id="remove-sauce-labs-fleece-jacket"]'
-    go_rembtn = wait.until(EC.element_to_be_clickable((By.XPATH, removebtn_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, removebtn_xpath)))
     base_page.click_element(By.XPATH, removebtn_xpath)
 
     # STEP7 - Press the "Checkout" button:
     checkout_btn_xpath = '/html/body/div/div/div/div[2]/div/div[2]/button[2]'
-    click_checkoutbtn = wait.until(EC.element_to_be_clickable((By.XPATH, checkout_btn_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, checkout_btn_xpath)))
     base_page.click_element(By.XPATH, checkout_btn_xpath)
 
     # STEP8 - Fill in the First Name, Last Name, and ZIP fields
@@ -131,17 +131,17 @@ def test_user_login_checkout(setup_driver):
 
     # STEP9 - Press the "Continue" button:
     continue_btn_xpath = '//*[@id="continue"]'
-    click_continuebtn = wait.until(EC.element_to_be_clickable((By.XPATH, continue_btn_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, continue_btn_xpath)))
     base_page.click_element(By.XPATH, continue_btn_xpath)
 
     # STEP10 - Press the "Finish" button:
     finish_btn_xpath = '//*[@id="finish"]'
-    click_finishbtn = wait.until(EC.element_to_be_clickable((By.XPATH, finish_btn_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, finish_btn_xpath)))
     base_page.click_element(By.XPATH, finish_btn_xpath)
 
     # STEP11 - Return to the homepage:
     backhome_btn_xpath = '//*[@id="back-to-products"]'
-    click_homebtn = wait.until(EC.element_to_be_clickable((By.XPATH, backhome_btn_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, backhome_btn_xpath)))
     base_page.click_element(By.XPATH, backhome_btn_xpath)
 
     print('Result-Positive')
