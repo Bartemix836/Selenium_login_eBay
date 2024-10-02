b51592f406887742e51e9b7a832eaedefec867cf
Bartemix836 <114190309+Bartemix836@users.noreply.github.com>
2024-10-02 23:47:45 +0200

    Update test_social_media.py

--- tests/test_social_media.py
+++ tests/test_social_media.py
@@ -70,22 +70,22 @@ def test_social_media_interactions(setup_driver):
 
     # STEP1 - Enter the username
     login_input_xpath = '//*[@id="user-name"]'
-    login_input = wait.until(EC.element_to_be_clickable((By.XPATH, login_input_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, login_input_xpath)))
     base_page.fill_text_field(By.XPATH, login_input_xpath, username)
 
     # STEP2 - Enter the password
     passwd_input_xpath = '//*[@id="password"]'
-    passwd_input = wait.until(EC.element_to_be_clickable((By.XPATH, passwd_input_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, passwd_input_xpath)))
     base_page.fill_text_field(By.XPATH, passwd_input_xpath, password)
 
     # STEP3 - Press the login button
     login_btn_xpath = '//*[@id="login-button"]'
-    login_button = wait.until(EC.element_to_be_clickable((By.XPATH, login_btn_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, login_btn_xpath)))
     base_page.click_element(By.XPATH, login_btn_xpath)
 
     # STEP4 - Click the Twitter icon
     twitter_link_xpath = '//a[@data-test="social-twitter"]'
-    twitter_link = wait.until(EC.element_to_be_clickable((By.XPATH, twitter_link_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, twitter_link_xpath)))
     driver.execute_script("arguments[0].removeAttribute('target');", twitter_link)  # Open in the same tab
     base_page.click_element(By.XPATH, twitter_link_xpath)
     time.sleep(4)
@@ -95,14 +95,14 @@ def test_social_media_interactions(setup_driver):
 
     # STEP6 - Click the Facebook icon
     facebook_link_xpath = '//a[@data-test="social-facebook"]'
-    facebook_link = wait.until(EC.element_to_be_clickable((By.XPATH, facebook_link_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, facebook_link_xpath)))
     driver.execute_script("arguments[0].removeAttribute('target');", facebook_link)  # Open in the same tab
     base_page.click_element(By.XPATH, facebook_link_xpath)
     time.sleep(4)
 
     # STEP7 - Accept cookies on Facebook
     cookies_xpath = '//*[@id="facebook"]/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[1]/div/div[1]/div/span/span'
-    cookies_element = wait.until(EC.element_to_be_clickable((By.XPATH, cookies_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, cookies_xpath)))
     base_page.click_element(By.XPATH, cookies_xpath)
     time.sleep(4)
 
@@ -111,7 +111,7 @@ def test_social_media_interactions(setup_driver):
 
     # STEP9 - Click the LinkedIn icon
     linkedin_link_xpath = '//a[@data-test="social-linkedin"]'
-    linkedin_link = wait.until(EC.element_to_be_clickable((By.XPATH, linkedin_link_xpath)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, linkedin_link_xpath)))
     driver.execute_script("arguments[0].removeAttribute('target');", linkedin_link)  # Open in the same tab
     base_page.click_element(By.XPATH, linkedin_link_xpath)
     time.sleep(4)
